7c0f8bde94fa8d6efb4c6d245afa482595180099
Igor Mateus Mordaski <igormordaski9@gmail.com>
2025-09-27 18:11:45 -0300

    Corrigindo as classes

--- backend/DTOs/RegisterDto.cs
+++ backend/DTOs/RegisterDto.cs
@@ -1,8 +1,8 @@
 public class RegisterDto
 {
-    public string Email { get; set; } = null!;
-    public string Senha { get; set; } = null!;
     public string Nome { get; set; } = null!;
+    public string Email { get; set; } = null!;
     public string Turma { get; set; } = null!;
+    public string Senha { get; set; } = null!;
     public string ConfirmarSenha { get; set; } = null!;
 }
